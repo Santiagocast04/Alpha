ec3852b8fb9bba58b8d2db86a0b3daed07a491ab
Fabio Andrés Castañeda <fcastaneda@picap.co>
2021-11-22 14:35:43 -0500

    add one ot many association betwen users and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_203803) do
+ActiveRecord::Schema.define(version: 2021_11_22_192211) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_18_203803) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "name_test"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
