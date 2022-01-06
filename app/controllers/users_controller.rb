d0ece34716a32a89bb83c9d2a4d050b47ddec752
Fabio Andrés Castañeda <fcastaneda@picap.co>
2022-01-06 11:55:52 -0500

    add users sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "welcome to the alpha blog, you have successfully signup"
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :emails, :password)
+  end
+
 end
