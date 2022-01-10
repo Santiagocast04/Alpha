a7d61fa6f1be771ed67ea05537c7853e5226c0f9
Fabio Andrés Castañeda <fcastaneda@picap.co>
2022-01-10 16:31:42 -0500

    add some test on the text folder to see what happen

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_08_215314) do
+ActiveRecord::Schema.define(version: 2022_01_10_202613) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2022_01_08_215314) do
     t.datetime "updated_at"
     t.string "name_test"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
